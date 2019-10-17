522b7f2ab5a9fbb830da183ab4224a36c2760655
Medyuk, Aleksandr <a.medyuk@a1qa.com>
2019-10-17 18:02:59 +0300

    delete creds

--- resources/vk_config.py
+++ resources/vk_config.py
@@ -1,4 +1,4 @@
-Login = "375292773875"
-Password = "Artur_pir0zk0v"
-Access_token = "ed3a3e6e0440dbafd802fb45c21c5f0cdfd5d94dd1424be07a9f638f83e4ad5a55cf2e5145157f63b3397"
+Login = ""
+Password = ""
+Access_token = ""
 Api_version = 5.102
